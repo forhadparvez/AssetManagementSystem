03770257642b212fc31d38061afc71102eb2609f
forhadparvez <forhadparvezfahim@gmail.com>
2017-09-28 13:22:15 +0600

    Asset Location Form

--- AssetManagementUI/Controllers/AssetLocationsController.cs
+++ AssetManagementUI/Controllers/AssetLocationsController.cs
@@ -1,24 +1,49 @@
-﻿using System.Web.Mvc;
-using Asset.BisnessLogic.Library.Organizations;
+﻿using Asset.Models.Library.EntityModels.OrganizationModels;
+using AssetManagementUI.ViewModels.AssetSetups;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace AssetManagementUI.Controllers
 {
     public class AssetLocationsController : Controller
     {
-        private readonly OrganizationManager _organizationManager;
-        private readonly BranchManager _branchManager;
-        //private readonly assetloc
         // GET: AssetLocations
         public ActionResult Index()
         {
             return View();
         }
 
-
-        [HttpGet]
-        public PartialViewResult Create()
+        public ActionResult New()
         {
-            return PartialView("PartialView/AssetLocations/_CreatePartialView");
+            var assetLocationVm = new AssetLocationViewModel()
+            {
+                Organizations = new List<Organization>()
+                {
+
+                },
+                Branches = new List<Branch>()
+                {
+
+                }
+            };
+            return View("AssetLocationForm", assetLocationVm);
         }
+
+        [HttpPost]
+        public ActionResult Save(AssetLocationViewModel assetLocationVm)
+        {
+            return View("AssetLocationForm");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View("Index");
+        }
+
     }
 }
